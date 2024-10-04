a3bbde6db10ce444f2f7e33d6f337ca4cfb2dd65
Malkaviala <128104470+Malkaviala@users.noreply.github.com>
2024-10-04 13:49:39 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBVH4syVcOVAJJI0BkPl8EWUcarmgqQIH0",
+    authDomain: "shipment-c50a1.firebaseapp.com",
+    projectId: "shipment-c50a1",
+    storageBucket: "shipment-c50a1.appspot.com",
+    messagingSenderId: "552502680496",
+    appId: "1:552502680496:web:5563feea124f54904d5475",
+    measurementId: "G-6C927VG178"
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+
